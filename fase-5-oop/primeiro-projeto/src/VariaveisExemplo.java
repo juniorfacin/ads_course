630d9c67aba28ba258af422bd610a61cf3d911b7
Junior Facin <jufacin@hotmail.com>
2024-08-08 08:02:37 -0300

    Starting phase 5: OOP - Variables and Operators

---
+++ fase-5-oop/primeiro-projeto/src/VariaveisExemplo.java
@@ -0,0 +1,18 @@
+public class VariaveisExemplo {
+    public static void main(String[] args) {
+        // Declarando variáveis e inicializando variáveis
+        int idade = 30;
+        double altura = 1.75;
+        char genero = 'M';
+        boolean ehEstudante = true;
+
+        // Exibindo os valores das variáveis
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Gênero: " + genero);
+        System.out.println("É estudante? " + ehEstudante);
+
+        // Outro método de exibir variáveis com o printf
+        System.out.printf("Idade: %d%n", idade);
+    }
+}
